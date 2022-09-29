7544704f9b97b3844bd38a5608b557e2559671a7
Shoaibv2 <skrj1337@gmail.com>
2022-09-29 21:59:17 +0500

    29 Sept

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className='row gx-3'>
+
+      <Card />
+      <Card />
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+function Card() {
+  return (
+    <div className="card w-25">
+      <img src="https://www.xda-developers.com/files/2018/06/pubg.jpg" className="card-img-top" alt="pubg" />
+      <div className="card-body">
+        <h5 classNameName="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="https://meet.google.com/nzy-dsxz-vsg" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  )
+}
+
+export default Card
